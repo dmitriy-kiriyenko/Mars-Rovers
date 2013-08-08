7c58c1abe4f5f4cb0b9ddd2d852630ed5873f2bc
Dmitriy Kiriyenko <dmitriy.kiriyenko@anahoret.com>
2013-08-08 15:59:46 +0300

    Nichego sebe.

--- mars-rovers.hs
+++ mars-rovers.hs
@@ -5,24 +5,24 @@ data Delta = Delta Int Int
 
 delta N = Delta   0    1
 delta E = Delta   1    0
-delta S = Delta (-1)   0
-delta W = Delta   0  (-1)
+delta S = Delta   0  (-1)
+delta W = Delta (-1)   0
 
 turn n e = toEnum (add (fromEnum (maxBound `asTypeOf` e) + 1) (fromEnum e) n)
     where add mod x y = (x + y + mod) `rem` mod
 move (Rover x y h) (Delta dx dy) = Rover (x + dx) (y + dy) h
 
-act   (Rover x y h) L = Rover x y (turn (-1) h)
-act   (Rover x y h) R = Rover x y (turn   1  h)
-act r@(Rover x y h) M = move r $ delta h
+act L   (Rover x y h) = Rover x y (turn (-1) h)
+act R   (Rover x y h) = Rover x y (turn   1  h)
+act M r@(Rover x y h) = move r $ delta h
 
-travel' rover commands = foldl act rover commands
+action commands = foldr (.) id $ reverse $ map act commands
 
 toList x = [x]
 readRover    input = read ("Rover " ++ input)
 readCommands input = map (read . toList) input
 
-travel rover commands = travel' (readRover rover) (readCommands commands)
+travel rover commands = action (readCommands commands) (readRover rover)
 
 work = do
   rover    <- getLine
